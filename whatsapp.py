46bb59cb4dc08d5e00eebcf6883faeb9c0d3642e
chobanivan999 <chobanivan999@gmail.com>
2023-12-11 03:09:29 +0200

    add scraping part

--- whatsapp.py
+++ whatsapp.py
@@ -1,5 +1,9 @@
 from flask import Flask, request
 from twilio.rest import Client
+import requests
+import json
+from bs4 import BeautifulSoup
+import mysql.connector
 from flask_cors import CORS
 
 # Configuring Twilio Credentials and Logging in
@@ -17,13 +21,81 @@ def send():
     phones = phones.split(",")
     print(phones)
     for phone in phones:
-        message = client.messages.create(
-            from_ = 'whatsapp:+601119083609',
-            body = msg,
-            to = 'whatsapp:' + phone
-        )
+        try:
+            client.messages.create(
+                from_ = 'whatsapp:+601119083609',
+                body = msg,
+                to = 'whatsapp:' + phone
+            )
+            print(phone, ": success")
+        except Exception as e:
+            print(phone, str(e))
 
-    return "success"
+    return "Sent"
+
+@app.route('/banks', methods=['GET'])
+def banks():
+    # Connect DB
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="helper-mortgage-option"
+    )
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT COUNT(*) FROM banks WHERE STR_TO_DATE(created_at, '%Y-%m-%d') >= DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY)")
+
+    myresult = mycursor.fetchall()
+
+    if len(myresult) == 0:
+        # Clear table
+        mycursor.execute("DELETE FROM banks")
+        mydb.commit()
+        # Scraping
+        URL = "https://mortgage.redbrick.sg/package_list/icecube"
+        page = requests.get(URL)
+
+        soup = BeautifulSoup(page.content, "html.parser")
+        rows = soup.findAll(True, {"class":["row row-content"]})
+        if len(rows):
+            rows = rows[1:51]
+
+        banklogos = {
+            'mbb': 'https://www.maybank.com/iwov-resources/images/icons/maybank_logo.svg',
+            'ocbc': 'https://www.ocbc.com/iwov-resources/sg/ocbc/gbc/img/logo_ocbc.png',
+            'dbs': 'https://www.dbs.com/iwov-resources/flp/images/dbs_logo.svg',
+            'hsbc': 'https://www.hsbc.com/-/files/hsbc/header/hsbc-logo-200x25.svg?h=25&la=en-GB&hash=EFB19274CD17649AE659D3351B595180',
+            'citibank': 'https://www.citi.com/CBOL/IA/Angular/assets/citiredesign.svg',
+            'scb': 'https://av.sc.com/corp-en/nr/content/images/sc-lock-up-english-rgb-thumbnail-750x422.jpg',
+            'cimb': 'https://www.cimbbank.com.ph/content/dam/cimbph/images/global/logo-cimb-islamic.svg',
+            'boc': 'https://www.boc.cn/en/images/bankofchina_LOGO.gif',
+            'hlf': 'https://www.hlf.com.sg/assets/images/home/logo.png',
+            'Singapura-Finance': 'https://www.singapurafinance.com.sg/img/logo.png',
+            'sbi': 'https://sbi.co.in/o/SBI-Theme/images/custom/logo.png'
+        }
+
+        for row in rows:
+            img = banklogos[row.img['src'].split("/")[-1].split(".")[0]]
+            ratetype = row.find(True, {"class":["package-type"]}).text.strip()
+            lockin = row.find(True, {"class":["package-lockin"]}).text.strip()
+            rate = row.find(True, {"class":["package-rate"]}).text.strip().split("\n")
+            rate = ' '.join([item.strip() for item in rate])
+            installment = row.find(True, {"class":["box__instalment"]}).text.strip().split("\n")
+            installment = ' '.join([item.strip() for item in installment if item != "Monthly Instalments"])
+            content = {
+                    "ratetype": ratetype,
+                    "lockin": lockin,
+                    "rate": rate,
+                    "installment": installment
+                }
+            mycursor.execute("INSERT INTO banks (image, content) VALUES (%s, %s)", (img, json.dumps(content)))
+        mydb.commit()
+
+    mycursor.execute("SELECT image, content FROM banks")
+    myresult = mycursor.fetchall()
+    print(myresult)
+    mydb.close()
 
 if __name__ == '__main__':
     app.run()
